5f8242fffa15c5e01f7e838b1d762906b126237c
gonzalo gisbert <gonzalo.a.gisbert@gmail.com>
2017-09-26 08:41:06 -0300

    adding the Provider to manage the store

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider>
+    <App />
+</Provider>, document.getElementById('root'));
+
 registerServiceWorker();
