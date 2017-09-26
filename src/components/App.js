1f2839fb913d9b8044605db9176a14c5e4c11486
gonzalo gisbert <gonzalo.a.gisbert@gmail.com>
2017-09-26 09:00:24 -0300

    adding the store into the index.js and the mapStateTotProps function in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { addRecipe } from '../actions'
+import { connect } from 'react-redux'
 
 class App extends Component {
 
@@ -12,4 +12,21 @@ class App extends Component {
     }
 }
 
-export default App
+function mapStateToProps (calendar) {
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+    return {
+        calendar: dayOrder.map((day) => ({
+            day,
+            meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+                meals[meal] = calendar[day][meal]
+                    ? calendar[day][meal]
+                    : null
+
+                return meals
+            }, {})
+        })),
+    }
+}
+
+export default connect(mapStateToProps)(App)
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = createStore(reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<Provider>
+ReactDOM.render(<Provider store={store}>
     <App />
 </Provider>, document.getElementById('root'));
 
